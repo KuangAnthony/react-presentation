aa2d29df1d5383078461f640c361ad42c753be9a
Anthony Kuang <kuanganthony@gmail.com>
2021-10-28 13:44:57 -0700

    first commit

---
+++ site.config.js
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: `@KuangAnthony`,
+  title: `<React /> - a dive into its ecosystem`,
+  date: `October 28, 2021`,
+  author: {
+    url: `https://twitter.com/KuangAnthony`,
+  },
+}
+
+export default siteConfig
